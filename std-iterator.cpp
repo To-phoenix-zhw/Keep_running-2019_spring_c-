22640b70cf19ff13a5be1426651ea517da08a988
zhw <1425619791@qq.com>
2019-05-28 21:47:57 +0800

    Create std-iterator.cpp

---
+++ std-iterator.cpp
@@ -0,0 +1,38 @@
+/*
+template <class Category, class T, class Distance = ptrdiff_t,
+          class Pointer = T*, class Reference = T&>
+  struct iterator {
+    typedef T         value_type;
+    typedef Distance  difference_type;
+    typedef Pointer   pointer;
+    typedef Reference reference;
+    typedef Category  iterator_category;
+  };
+*/
+// std::iterator example
+#include <iostream>     // std::cout
+#include <iterator>     // std::iterator, std::input_iterator_tag
+
+class MyIterator : public std::iterator<std::input_iterator_tag, int>
+{
+  int* p;
+public:
+  MyIterator(int* x) :p(x) {}
+  MyIterator(const MyIterator& mit) : p(mit.p) {}
+  MyIterator& operator++() {++p;return *this;}
+  MyIterator operator++(int) {MyIterator tmp(*this); operator++(); return tmp;}
+  bool operator==(const MyIterator& rhs) const {return p==rhs.p;}
+  bool operator!=(const MyIterator& rhs) const {return p!=rhs.p;}
+  int& operator*() {return *p;}
+};
+
+int main () {
+  int numbers[]={10,20,30,40,50};
+  MyIterator from(numbers);
+  MyIterator until(numbers+5);
+  for (MyIterator it=from; it!=until; it++)
+    std::cout << *it << ' ';
+  std::cout << '\n';
+
+  return 0;
+}
